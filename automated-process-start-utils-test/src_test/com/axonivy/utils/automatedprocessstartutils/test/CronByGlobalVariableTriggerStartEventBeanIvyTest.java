b7e74c7ff2249b6cea54b7498698c2bdf4129186
Majed Hmamad <majed.hmamad@axonivy.com>
2023-01-25 15:46:28 +0100

    MARKET-272 Cron Job Process Start Connector

--- automated-process-start-utils-test/src_test/com/axonivy/utils/automatedprocessstartutils/test/CronByGlobalVariableTriggerStartEventBeanIvyTest.java
+++ automated-process-start-utils-test/src_test/com/axonivy/utils/automatedprocessstartutils/test/CronByGlobalVariableTriggerStartEventBeanIvyTest.java
@@ -1,11 +1,16 @@
 package com.axonivy.utils.automatedprocessstartutils.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
+import com.axonivy.utils.automatedprocessstartutils.demo.BusinessCaseDemo;
+
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.environment.IvyTest;
+import ch.ivyteam.ivy.scripting.objects.DateTime;
 
 /**
  * This UnitTest is testing the demo process.
@@ -14,10 +19,12 @@ import ch.ivyteam.ivy.environment.IvyTest;
 public class CronByGlobalVariableTriggerStartEventBeanIvyTest {
 
 	@Test
-	public void afterPropertiesSetJobTestJobDetailAndTriggerNotNull() throws Exception {
-		TimeUnit.SECONDS.sleep(1);
-		Ivy.datacache().getSessionCache().getEntry("com.axonivy.utils.automatedprocessstartutils", "demoStartTime")
-				.isValid();
+	public void testCronJongStarted() throws Exception {
+		TimeUnit.SECONDS.sleep(5);
+		BusinessCaseDemo bCaseDemo = Ivy.repo().get(BusinessCaseDemo.class);
+		assertThat(bCaseDemo).isNotNull();
+		assertThat(bCaseDemo.getStartTime()).as("Cron job did not started").isNotNull();
+		assertThat(bCaseDemo.getStartTime()).as("Cron job did not started in the past", bCaseDemo.getStartTime()).isLessThan(new DateTime());
 	}
 
 }
